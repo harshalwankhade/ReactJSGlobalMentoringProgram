1d0c56ca422f6778751df39ea9eb1927429c9003
harshalwankhade <59637078+harshalwankhade@users.noreply.github.com>
2022-10-18 04:14:54 +0530

    Render blank message (Hello World) with React

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,10 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const paragraph = React.createElement('p', {}, 'Hello World');
+const container = React.createElement('div', {}, [paragraph]);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  container
 );
 
 // If you want to start measuring performance in your app, pass a function
